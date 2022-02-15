40a7c6044aa7ca4b2bfba4134d4a9a8dc045c849
Junwoo Lee <jlee648@myseneca.ca>
2022-02-14 23:26:02 -0500

    fix typo

--- src/About.js
+++ src/About.js
@@ -12,7 +12,7 @@ export default function About() {
                     <h4>Junwoo Lee</h4><br/>
                     <h5>Junior Developer</h5><br/>
                     <p>Currently in last semester in college and looking for a job to work as juniordeveloper or software engineer.<br/> Can work from May 2022 after graduation.
-                    Thinking various ideas for IT business, and training myself continuouslythrough online courses.</p>
+                    Thinking various ideas for IT business, and training myself continuously through online courses.</p>
                     </Card.Text>
                 </Card.Body>
             </Card>
